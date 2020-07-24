9890487b90f99c9f8564fbd315d9f05ae17ebb0d
Sayat <sayat.petrosyan@teamable.com>
2020-07-24 21:43:30 +0400

    till 1.7

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+h = 100
+
+
+for _ in range(10):
+    h = 3 * h / 5
+    print(round(h, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
+
 # Exercise 1.7
+
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+
+extra_payment_start_month = int(input('start month: '))
+extra_payment_end_month = int(input('end month: '))
+extra_payment = int(input('extra payment: '))
+
+while principal > 0:
+    if month > extra_payment_start_month and month < extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    principal = principal * (1+rate/12) - payment
+    if principal < 0:
+        payment = payment + principal
+        principal = 0
+    total_paid = total_paid + payment
+    month += 1
+    print(month, total_paid, principal)
+
+print('Total paid', total_paid,'\n', 'over', month, 'months')
